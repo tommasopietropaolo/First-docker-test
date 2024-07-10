7fda78aff9f33cf81886d62a688ea1058a41357b
Tommaso Pietropaolo <42649989+tommasopietropaolo@users.noreply.github.com>
2024-07-10 12:36:33 +0200

    Add files via upload

---
+++ MyFirstDockerApp/src/test/java/com/app/MyFirstDockerApp/MyFirstDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.MyFirstDockerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstDockerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
